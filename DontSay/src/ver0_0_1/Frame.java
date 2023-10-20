0ba667b8a055b38555419f68bd696270ed901333
darecoco <whaqlrh37@naver.com>
2023-10-20 23:53:50 +0900

    Initail commit

---
+++ DontSay/src/ver0_0_1/Frame.java
@@ -0,0 +1,21 @@
+package ver0_0_1;
+
+import javax.swing.*;
+
+//창만 만드는 코드(맵을 보여주는 코드는 Map.java)
+
+public class Frame {
+	private JFrame base = new JFrame();
+	Frame(){
+		base.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		base.setTitle("미림 금칙어");
+		base.setSize(1000, 1000);
+		base.setLayout(null);
+		base.setLocationRelativeTo(null);
+		base.setVisible(true);
+	}
+	
+	JFrame getFrame() {
+		return base;
+	}
+}
---
+++ DontSay/src/ver0_0_1/Map.java
@@ -0,0 +1,8 @@
+package ver0_0_1;
+
+//맵이 바뀌는 것을 판단하고 보여주는 코드.
+//Frame.java의 JFrame에 배경 컴포넌트를 add시키는 방식으로 진행.
+
+public class Map {
+	
+}
